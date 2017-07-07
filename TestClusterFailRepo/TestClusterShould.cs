bf8e9e1d2a7b7df5501c445ddee6c49ffd48ac59
Ian Bebbington <github@bebbs.co.uk>
2017-07-07 12:04:04 +0100

    Initial commit of TestClusterFailRepo

---
+++ TestClusterFailRepo/TestClusterShould.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Orleans.TestingHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClusterFailRepo
+{
+    [TestFixture]
+    public class TestClusterShould
+    {
+        [Test]
+        public void DeployWithoutAdditionalConfiguration()
+        {
+            var cluster = new TestCluster();
+            cluster.Deploy();
+
+            cluster.StopAllSilos();
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void DeployWithAdditionalConfiguration()
+        {
+            var cluster = new TestCluster(
+                new TestClusterOptions
+                {
+                    ExtendedFallbackOptions = new TestClusterOptions.FallbackOptions
+                    {
+                        InitialSilosCount = 1,
+                        TraceToConsole = true
+                    }
+                }
+            );
+
+            cluster.Deploy();
+
+            cluster.StopAllSilos();
+
+            Assert.Pass();
+        }
+    }
+}
