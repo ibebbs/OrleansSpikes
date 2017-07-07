ab0d78c58a1e534d2862689879c7024c62980933
Ian Bebbington <github@bebbs.co.uk>
2017-07-07 12:12:17 +0100

    Added Xunit version of TestClusterFailRepo which runs tests successfully.

---
+++ TestClusterFailRepoXunit/TestClusterShould.cs
@@ -0,0 +1,41 @@
+﻿using Orleans.TestingHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestClusterFailRepoXunit
+{
+    public class TestClusterShould
+    {
+        [Fact]
+        public void DeployWithoutAdditionalConfiguration()
+        {
+            var cluster = new TestCluster();
+            cluster.Deploy();
+
+            cluster.StopAllSilos();
+        }
+
+        [Fact]
+        public void DeployWithAdditionalConfiguration()
+        {
+            var cluster = new TestCluster(
+                new TestClusterOptions
+                {
+                    ExtendedFallbackOptions = new TestClusterOptions.FallbackOptions
+                    {
+                        InitialSilosCount = 1,
+                        TraceToConsole = true
+                    }
+                }
+            );
+
+            cluster.Deploy();
+
+            cluster.StopAllSilos();
+        }
+    }
+}
